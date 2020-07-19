19872624bb1721378d9be0148f1978d740a94aa8
Aniel-Feyt <anielfeyt@gmail.com>
2020-07-19 20:47:32 +0200

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
-
-app.get('/', (req, res) => 
-    res.json({ msg: 'Welcome to the ContactKeeper API...' })
-);
 
 // Connect Database
 connectDB();
@@ -17,6 +14,14 @@ app.use(express.json({ extended: false }));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
